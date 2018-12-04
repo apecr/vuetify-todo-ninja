7ec16f1dcbb910b567a6cf8fb9bf112acc925581
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-04 14:48:19 +0100

    lesson 5 buttons and icons
    [FA-139]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia minus dolores a distinctio asperiores culpa pariatur nobis corporis vitae, quis neque architecto rerum provident rem, placeat deleniti veritatis quo fuga!</p>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo eligendi ex nostrum esse qui, ducimus ea reprehenderit debitis illum perspiciatis tempora laboriosam, dolor adipisci alias earum nam itaque iste totam.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold text-uppercase">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn color="pink" flat>click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon>favorite</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
