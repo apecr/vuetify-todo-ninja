24f150f444080f7601bc9fb6d9b97a57df4ad19d
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-04 15:29:46 +0100

    lesson 10 lists with links
    [FA-139]

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <v-toolbar flat app>
-        <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -14,7 +14,16 @@
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
-      <p class="error">test</p>
+      <v-list>
+        <v-list-tile v-for="(link, index) in links" :key="index" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -24,7 +33,12 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "folder", text: "My Projects", route: "/projects" },
+        { icon: "person", text: "Team", route: "/team" }
+      ]
     };
   }
 };
