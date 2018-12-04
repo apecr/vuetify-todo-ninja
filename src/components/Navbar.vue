83dcf2970cf8755081f6b9547589312208843c67
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-04 15:13:05 +0100

    lesson 8 drawer
    [FA-139]

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,31 @@
 <template>
   <nav>
     <v-toolbar flat app>
+        <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn flat color="grey">
-          <span>Sign Out</span>
-          <v-icon right>exit_to_app</v-icon>
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <p>test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false
+    };
+  }
+};
 </script>
