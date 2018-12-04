78f8722f308403e02e9fbfaa8fce9bd8d2242152
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-04 16:41:46 +0100

    lesson 14 grid II
    [FA-139]

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,18 +2,34 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-      </v-layout>
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-flex>
+          <v-flex xs6 md2 sm4>
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-flex>
+          <v-flex xs6 md2 sm4>
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-flex>
+          <v-flex xs2 md2 sm4>
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {}
+  }
+};
 </script>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,20 +2,6 @@
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
     <v-container class="my-5">
-      <v-layout row wrap>
-        <v-flex xs12 md6>
-          <v-btn outline block class="primary">1</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">2</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">3</v-btn>
-        </v-flex>
-        <v-flex xs4 md2>
-          <v-btn outline block class="primary">4</v-btn>
-        </v-flex>
-      </v-layout>
     </v-container>
   </div>
 </template>
