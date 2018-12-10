026978da955c5a78af3af822747f234c2cf9168a
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-10 17:11:22 +0100

    Include cards for team members
    [FA-139]

--- README.md
+++ README.md
@@ -46,3 +46,4 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 * Grid System --> https://vuetifyjs.com/en/layout/grid#introduction
 * Chips --> https://vuetifyjs.com/en/components/chips#introduction
 * Tooltip --> https://vuetifyjs.com/en/components/tooltips#introduction
+* Cards --> https://vuetifyjs.com/en/components/cards#introduction
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,10 +3,39 @@
     <h1 class="subheading grey--text">Team</h1>
 
     <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              image goes here
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{person.name}}</div>
+              <div class="grey--text">{{person.role}}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    team: [
+      { name: "The Net Ninja", role: "Web developer" },
+      { name: "Ryu", role: "Graphic designer" },
+      { name: "Chun Li", role: "Web developer" },
+      { name: "Gouken", role: "Social media maverick" },
+      { name: "Yoshi", role: "Sales guru" }
+    ]
+  })
+};
 </script>
