563d53b263102e88f99cec64848ca8f0ce9241e4
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-12-12 10:58:21 +0100

    Inlcude form validation
    [FA-139]

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -6,9 +6,9 @@
         <h2>Add a New Project</h2>
       </v-card-title>
       <v-card-text>
-        <v-form class="px-3">
-          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        <v-form class="px-3" ref="form">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
           <v-menu>
             <v-text-field
               :value="formattedDate"
@@ -33,16 +33,19 @@ export default {
   data: () => ({
     title: null,
     content: null,
-    due: null
+    due: null,
+    inputRules: [v => (v && v.length >= 3) || "Minimum length is 3 characters"]
   }),
   methods: {
     createProject() {
-      console.log(this.title, this.content, this.due);
+      if (this.$refs.form.validate()) {
+        console.log(this.title, this.content, this.due);
+      }
     }
   },
   computed: {
     formattedDate() {
-      return this.due ? format(this.due, 'Do MMM YYYY') : "";
+      return this.due ? format(this.due, "Do MMM YYYY") : "";
     }
   }
 };
